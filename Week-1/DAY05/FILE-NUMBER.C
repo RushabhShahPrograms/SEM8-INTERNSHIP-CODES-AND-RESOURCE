a16104f50f5e60c7043ba2e60cd8622e06b1eb47
Rushabh Shah <90546286+RushabhShahPrograms@users.noreply.github.com>
2023-02-09 16:18:28 +0530

    Create FILE-NUMBER.C

---
+++ Week-1/DAY05/FILE-NUMBER.C
@@ -0,0 +1,17 @@
+/*
+Author: Rushabh Shah
+Date of Creation: 9th Feb 2023
+Subject: To read and write the numbers in file
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  FILE *fp1,*fp2;
+  int num;
+  clrscr();
+  
+  fp1=
+}
