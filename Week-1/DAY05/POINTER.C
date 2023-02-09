b4cdbefd7fc326f86cfb38647e0ec000c46d530a
Rushabh Shah <90546286+RushabhShahPrograms@users.noreply.github.com>
2023-02-09 15:54:44 +0530

    Create POINTER.C

---
+++ Week-1/DAY05/POINTER.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  int x=9,z=10;
+  float *y;
+  float a[10];
+  int i;
+  clrscr();
+  /*
+    y=&x;
+    printf("\n\n%d----%u",*y,y);
+    
+    y=&z;
+    printf("\n\n%d----%u",*y,y);
+  */
+  y=a;
+  for(i=0;i<10;i++)
+  {
+    printf("\nEnter the value: ");
+    scanf("%f",y+i);
+  }
+  
+  clrscr();
+  for(i=0;i<10;i++)
+  {
+    printf("\nThe value is %f and address is %u",*(y+i),y+i);
+  }
+  getch();
+}
