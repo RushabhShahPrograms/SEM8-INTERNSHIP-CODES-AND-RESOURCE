d6797950add96f119c9ac889c6c5abc12db8ecb0
Rushabh Shah <90546286+RushabhShahPrograms@users.noreply.github.com>
2023-02-09 16:35:51 +0530

    Update FILE-NUMBER.C

--- Week-1/DAY05/FILE-NUMBER.C
+++ Week-1/DAY05/FILE-NUMBER.C
@@ -10,8 +10,39 @@ Subject: To read and write the numbers in file
 void main()
 {
   FILE *fp1,*fp2;
-  int num;
+  int num,i;
   clrscr();
   
-  fp1=
+  fp1=fopen("even.txt","w");
+  fp2=fopen("odd.txt","w");
+  for(i=0;i<10;i++)
+  {
+    printf("\nEnter the number");
+    scanf("%d",&num);
+    if(num%2==0)
+    {
+      putw(num,fp1);
+    }
+    else
+    {
+      putw(num,fp2);
+    }
+  }
+  fclose(fp1);
+  fclose(fp2);
+  fp1=fopen("even.txt","r");
+  fp2=fopen("odd.txt","r");
+  printf("\nThe even numbers are as follows: ");
+  while(num=getw(fp1))!=EOF)
+  {
+    printf("%d\t",num);
+  }
+  fclose(fp1);
+  printf("\n The odd numbers are as follows: ");
+  while((num=getw(fp2))!=EOF)
+  {
+    printf("%d\t",num);
+  }
+  fclose(fp2);
+  getch();
 }
