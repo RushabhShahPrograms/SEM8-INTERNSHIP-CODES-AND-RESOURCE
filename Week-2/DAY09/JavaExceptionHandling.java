e1e3dcfa56534cfe8f178497edef639d92557a57
Rushabh Shah <90546286+RushabhShahPrograms@users.noreply.github.com>
2023-02-16 14:10:10 +0530

    Create JavaExceptionHandling.java

---
+++ Week-2/DAY09/JavaExceptionHandling.java
@@ -0,0 +1,60 @@
+/*
+Author: Rushabh Shah
+Date of Creation: 15th Feb 2023
+Subject: Exception Handling in Java
+*/
+
+package javaapplication2;
+import java.util.Date;
+class SundayException extends Exception
+{
+    public SundayException(String msg)
+    {
+        super(msg);
+    }
+}
+public class ExceptionandHandling 
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            Date obj=new Date();
+            String str=obj.toString().substring(0,3);
+            if(str.equalsIgnoreCase("thur"))
+            {
+                throw new SundayException("Today Holiday");
+            }
+            int x=Integer.parseInt(args[0]);
+            int y=Integer.parseInt(args[1]);
+            int z=x/y;
+            System.out.println(z);
+        }
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Enter min two arguments");
+            e.printStackTrace();
+        }
+        catch(NumberFormatException e)
+        {
+            System.out.println("Please enter two integer");
+            e.printStackTrace();
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("please avoid second argument as zero");
+        }
+        catch(SundayException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            System.out.println("Don't Ride Bike");
+        }
+    }
+}
